fe9d28e3928bbbb949086749c6f5088e1ad02fee
Cody Thomas <codybthomas@gmail.com>
2018-12-09 15:52:38 -0800

    Added in a regex to update the response info to be properly formatted in the UI with newlines

--- app/templates/share_task.js
+++ app/templates/share_task.js
@@ -18,6 +18,9 @@ function set_info(response){
         task_info.callback = data['callback'];
         task_info.task = data['task'];
         task_info.responses = data['responses'];
+        for(var i = 0; i < task_info.responses.length; i++){
+            task_info.responses[i]['response'] = task_info.responses[i]['response'].replace(/\\n|\r/g, '\n');
+        }
     }
 }
 httpGetAsync("{{http}}://{{links.server_ip}}:{{links.server_port}}{{links.api_base}}/tasks/{{tid}}", set_info, "GET", null);
