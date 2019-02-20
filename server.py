e10605b38d46888bc96a135d64a6aa18a6952d29
Cody Thomas <codybthomas@gmail.com>
2019-02-19 21:15:29 -0800

    Added some code to get the error back if we try to run Apfell on a port that's already bound to something else

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from app import apfell, dbloop, apfell_db, db_objects, use_ssl, listen_port, lis
 import asyncio
 import ssl
 
+
 if __name__ == "__main__":
     asyncio.set_event_loop(dbloop)
     if use_ssl:
@@ -12,7 +13,18 @@ if __name__ == "__main__":
         server = apfell.create_server(host=listen_ip, port=listen_port, debug=False)
     loop = asyncio.get_event_loop()
     task = asyncio.ensure_future(server)
-    db_objects.database.allow_sync = True  # raise AssertionError on ANY sync call, needs to be worked out though
+
+    # thanks to some awesome people at the sanic community forums,
+    # we can now detect when the bound port is already in use
+    def callback(fut):
+        try:
+            fetch_count = fut.result()
+        except OSError as e:
+            print("probably the port set is being used")
+            fut.get_loop().stop()
+    task.add_done_callback(callback)
+
+    db_objects.database.allow_sync = True
     try:
         loop.run_until_complete(apfell_db.connect_async(loop=dbloop))
         loop.run_forever()
