f6a20503a49bebcef76218702b2154da96ede3d9
Cody Thomas <codybthomas@gmail.com>
2019-05-01 19:42:08 -0700

    updated the execution order of install scripts

--- install_docker_debian.sh
+++ install_docker_debian.sh
@@ -24,6 +24,3 @@ apt-get install -y -qq --no-install-recommends docker-ce >/dev/null
 curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 chmod +x /usr/local/bin/docker-compose
-
-# now that everything is installed, kick off our setup script
-./setup_apfell.sh
