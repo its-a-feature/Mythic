58edfdafab8fb6c04856c14a2b5788f03b78109b
Cody Thomas <codybthomas@gmail.com>
2020-10-05 21:47:41 -0700

    Revert "fixed atlas upload/download"
    This reverts commit 3264171a0c7ce48b155b455508fcea8caa0b5cf1.

--- Payload_Types/atlas/mythic/agent_functions/download.py
+++ Payload_Types/atlas/mythic/agent_functions/download.py
@@ -5,11 +5,20 @@ import json
 class DownloadArguments(TaskArguments):
     def __init__(self, command_line):
         super().__init__(command_line)
-        self.args = {}
+        self.args = {
+            "file_path": CommandParameter(
+                name="file_path",
+                type=ParameterType.String,
+                description="Path to remote file to be downloaded",
+            )
+        }
 
     async def parse_arguments(self):
         if len(self.command_line) > 0:
-            pass
+            if self.command_line[0] == "{":
+                self.load_args_from_json_string(self.command_line)
+            else:
+                self.add_arg("path", self.command_line)
         else:
             raise ValueError("Missing arguments")
 
@@ -23,7 +32,7 @@ class DownloadCommand(CommandBase):
     is_exit = False
     is_file_browse = False
     is_process_list = False
-    is_download_file = False
+    is_download_file = True
     is_remove_file = False
     is_upload_file = False
     author = ""
