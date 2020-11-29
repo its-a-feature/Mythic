2f71fd75e8d42159074f9b446e0cc33f75dd4b6b
Cody Thomas <codybthomas@gmail.com>
2020-11-29 12:07:08 -0800

    fixed a name collision issue with the example C2RPC code due to multiple import * statements

--- C2_Profiles/HTTP/mythic/c2_functions/C2_RPC_functions.py
+++ C2_Profiles/HTTP/mythic/c2_functions/C2_RPC_functions.py
@@ -1,5 +1,5 @@
 from C2ProfileBase import *
-from MythicCallbackRPC import *
+import MythicCallbackRPC
 
 # request is a dictionary: {"action": func_name, "message": "the input",  "task_id": task id num}
 # must return an RPCResponse() object and set .status to an instance of RPCStatus and response to str of message
@@ -7,4 +7,5 @@ async def test(request):
     response = RPCResponse()
     response.status = RPCStatus.Success
     response.response = "hello"
+    #resp = await MythicCallbackRPC.MythicCallbackRPC().add_event_message(message="got a POST message")
     return response
