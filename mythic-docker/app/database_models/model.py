11f03c9e2d1f91bbdc327ec9af6264eafa9dfda1
Thiago Mayllart <14459910+thiagomayllart@users.noreply.github.com>
2021-01-02 04:53:55 -0300

    Update model.py

--- mythic-docker/app/database_models/model.py
+++ mythic-docker/app/database_models/model.py
@@ -725,7 +725,6 @@ class Callback(p.Model):
     # associated socks information
     port = p.IntegerField(null=True)
     socks_task = p.DeferredForeignKey("Task", null=True)
-    rportfwd_task = p.DeferredForeignKey("Task", null=True)
     # if you need to define extra context for a callback, like a webshell, supply that here
     extra_info = p.TextField(null=False, default="")
     # store information about sleep interval/jitter/waking hours/etc here
