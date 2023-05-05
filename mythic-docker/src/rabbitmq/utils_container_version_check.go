4966250166af3fbe60d1773760761e775dfb55fc
its-a-feature <codybthomas@gmail.com>
2023-05-05 11:38:50 -0500

    update version check
    update the version check for containers to support patch updates

--- mythic-docker/src/rabbitmq/utils_container_version_check.go
+++ mythic-docker/src/rabbitmq/utils_container_version_check.go
@@ -5,12 +5,12 @@ import (
 	"golang.org/x/mod/semver"
 )
 
-const validContainerVersionMax = "v1.0"
+const validContainerVersionMax = "v1.1"
 
 func isValidContainerVersion(version string) bool {
 	if semver.IsValid(validContainerVersionMax) {
 		if semver.IsValid(version) {
-			if semver.Compare(version, validContainerVersionMax) <= 0 {
+			if semver.Compare(version, validContainerVersionMax) < 0 {
 				return true
 			} else {
 				logging.LogError(nil, "attempt to sync a container out of bounds", "version", version)
