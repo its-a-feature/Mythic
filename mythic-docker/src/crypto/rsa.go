69854ee85e89469e980031d2f8a0dc5a732b1050
Edznux <edznux@gmail.com>
2023-05-17 00:47:54 +0200

    Refactor and add test for rsa/crypto generation

--- mythic-docker/src/crypto/rsa.go
+++ mythic-docker/src/crypto/rsa.go
@@ -6,65 +6,58 @@ import (
 	"crypto/sha1"
 	"crypto/x509"
 	"encoding/pem"
-	"errors"
 	"fmt"
-
-	"github.com/its-a-feature/Mythic/logging"
 )
 
 func GenerateRSAKeyPair() ([]byte, *rsa.PrivateKey, error) {
-	if serverPrivKey, err := rsa.GenerateKey(rand.Reader, 4096); err != nil {
-		logging.LogError(err, "Failed to generate a new RSA keypair")
-		errorString := fmt.Sprintf("Failed to generate a new RSA keypair: %s", err.Error())
-		return nil, nil, errors.New(errorString)
-	} else {
-		serverPubKey := &serverPrivKey.PublicKey
-		pubASN1 := x509.MarshalPKCS1PublicKey(serverPubKey)
-		pubPem := pem.EncodeToMemory(
-			&pem.Block{
-				Type:  "RSA PUBLIC KEY",
-				Bytes: pubASN1,
-			},
-		)
-		return pubPem, serverPrivKey, nil
+	serverPrivKey, err := rsa.GenerateKey(rand.Reader, 4096)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to generate a new RSA keypair: %v", err)
 	}
+
+	serverPubKey := &serverPrivKey.PublicKey
+	pubASN1 := x509.MarshalPKCS1PublicKey(serverPubKey)
+	pubPem := pem.EncodeToMemory(
+		&pem.Block{
+			Type:  "RSA PUBLIC KEY",
+			Bytes: pubASN1,
+		},
+	)
+	return pubPem, serverPrivKey, nil
 }
 
 func RsaDecryptCipherBytes(encryptedData []byte, privateKey *rsa.PrivateKey) ([]byte, error) {
 	hash := sha1.New()
-	if decryptedData, err := rsa.DecryptOAEP(hash, rand.Reader, privateKey, encryptedData, nil); err != nil {
-		logging.LogError(err, "Failed to decrypt with RSA private key")
-		stringErr := errors.New(fmt.Sprintf("Failed to decrypt with RSA private key: %s", err.Error()))
-		return nil, stringErr
-	} else {
-		return decryptedData, nil
+	decryptedData, err := rsa.DecryptOAEP(hash, rand.Reader, privateKey, encryptedData, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decrypt with RSA private key: %v", err)
 	}
+
+	return decryptedData, nil
 }
 
 func RsaEncryptBytes(plainBytes []byte, publicKey []byte) ([]byte, error) {
 	hash := sha1.New()
 	//logging.LogInfo("about to parse public key in RsaEncryptBytes", "public key", publicKey)
-	if pkcs1RSAPublicKey, _ := pem.Decode(publicKey); pkcs1RSAPublicKey == nil {
-		logging.LogError(nil, "Failed to find PEM encoded public key")
-		return nil, errors.New("Failed to find PEM encoded public key")
-	} else {
-		var pubKey *rsa.PublicKey
-		var err error
-		if pubKey, err = x509.ParsePKCS1PublicKey(pkcs1RSAPublicKey.Bytes); err != nil {
-			if pubAny, err := x509.ParsePKIXPublicKey(pkcs1RSAPublicKey.Bytes); err != nil {
-				logging.LogError(err, "Failed to parse public key to encrypt with RSA")
-				errorString := fmt.Sprintf("Failed to parse public key to encrypt with RSA: %s", err.Error())
-				return nil, errors.New(errorString)
-			} else {
-				pubKey = pubAny.(*rsa.PublicKey)
-			}
-		}
-		if encryptedData, err := rsa.EncryptOAEP(hash, rand.Reader, pubKey, plainBytes, nil); err != nil {
-			logging.LogError(err, "Failed to encrypt with RSA key")
-			errorString := fmt.Sprintf("Failed to encrypt with RSA key: %s", err.Error())
-			return nil, errors.New(errorString)
-		} else {
-			return encryptedData, nil
-		}
+	pkcs1RSAPublicKey, _ := pem.Decode(publicKey)
+	if pkcs1RSAPublicKey == nil {
+		return nil, fmt.Errorf("failed to find PEM encoded public key")
 	}
+
+	pubKey, err := x509.ParsePKCS1PublicKey(pkcs1RSAPublicKey.Bytes)
+	if err != nil {
+		// Fallback to parsing PKIX instead of PKCS1
+		pubAny, err := x509.ParsePKIXPublicKey(pkcs1RSAPublicKey.Bytes)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse public key to encrypt with RSA: %v", err)
+		}
+		pubKey = pubAny.(*rsa.PublicKey)
+	}
+
+	encryptedData, err := rsa.EncryptOAEP(hash, rand.Reader, pubKey, plainBytes, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to encrypt with RSA key: %v", err)
+	}
+
+	return encryptedData, nil
 }
