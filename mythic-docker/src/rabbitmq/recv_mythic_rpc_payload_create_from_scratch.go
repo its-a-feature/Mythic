8bfc904dc61f3bc2761db0e3e78f102ed0359e9b
its-a-feature <codybthomas@gmail.com>
2024-04-05 17:55:17 -0500

    v3.2.20-rc8
    fixing MythicRPC issue for generating payloads from scratch

--- mythic-docker/src/rabbitmq/recv_mythic_rpc_payload_create_from_scratch.go
+++ mythic-docker/src/rabbitmq/recv_mythic_rpc_payload_create_from_scratch.go
@@ -26,9 +26,9 @@ type MythicRPCPayloadCreateFromScratchMessageResponse struct {
 func init() {
 	RabbitMQConnection.AddRPCQueue(RPCQueueStruct{
 		Exchange:   MYTHIC_EXCHANGE,
-		Queue:      MYTHIC_RPC_PAYLOAD_CREATE_FROM_SCRATCH, // swap out with queue in rabbitmq.constants.go file
-		RoutingKey: MYTHIC_RPC_PAYLOAD_CREATE_FROM_SCRATCH, // swap out with routing key in rabbitmq.constants.go file
-		Handler:    processMythicRPCPayloadCreateFromUUID,  // points to function that takes in amqp.Delivery and returns interface{}
+		Queue:      MYTHIC_RPC_PAYLOAD_CREATE_FROM_SCRATCH,   // swap out with queue in rabbitmq.constants.go file
+		RoutingKey: MYTHIC_RPC_PAYLOAD_CREATE_FROM_SCRATCH,   // swap out with routing key in rabbitmq.constants.go file
+		Handler:    processMythicRPCPayloadCreateFromScratch, // points to function that takes in amqp.Delivery and returns interface{}
 	})
 }
 
@@ -38,38 +38,40 @@ func MythicRPCPayloadCreateFromScratch(input MythicRPCPayloadCreateFromScratchMe
 		Success: false,
 	}
 	task := databaseStructs.Task{}
-	if err := database.DB.Get(&task, `SELECT operator_id, 
-	callback.operation_id "callback.operation_id"
+	if err := database.DB.Get(&task, `SELECT 
+    operator_id, operation_id
 	FROM task 
-	JOIN callback ON task.callback_id = callback.id
 	WHERE id=$1`, input.TaskID); err != nil {
 		logging.LogError(err, "Failed to get operator_id from task when generating payload")
 		response.Error = err.Error()
 		return response
 	}
-	if newUUID, newID, err := RegisterNewPayload(input.PayloadConfiguration, &databaseStructs.Operatoroperation{
-		CurrentOperation: databaseStructs.Operation{ID: task.Callback.OperationID},
+	newUUID, newID, err := RegisterNewPayload(input.PayloadConfiguration, &databaseStructs.Operatoroperation{
+		CurrentOperation: databaseStructs.Operation{ID: task.OperationID},
 		CurrentOperator:  databaseStructs.Operator{ID: task.OperatorID},
-	}); err != nil {
+	})
+	if err != nil {
 		response.Error = err.Error()
 		return response
-	} else if _, err := database.DB.Exec(`UPDATE payload SET auto_generated=true WHERE id=$1`, newID); err != nil {
+	}
+	_, err = database.DB.Exec(`UPDATE payload SET auto_generated=true WHERE id=$1`, newID)
+	if err != nil {
 		logging.LogError(err, "failed to update payload auto_generated status")
 		response.Error = err.Error()
 		return response
-	} else {
-		if input.RemoteHost != nil {
-			if _, err := database.DB.Exec(`INSERT INTO payloadonhost 
+	}
+	if input.RemoteHost != nil {
+		if _, err := database.DB.Exec(`INSERT INTO payloadonhost 
 					(host, payload_id, operation_id, task_id) 
 					VALUES 
-					($1, $2, $3, $4)`, strings.ToUpper(*input.RemoteHost), newID, task.Callback.OperationID, input.TaskID); err != nil {
-				logging.LogError(err, "Failed to register payload on host")
-			}
+					($1, $2, $3, $4)`, strings.ToUpper(*input.RemoteHost), newID, task.OperationID, input.TaskID); err != nil {
+			logging.LogError(err, "Failed to register payload on host")
 		}
-		response.NewPayloadUUID = newUUID
-		response.Success = true
-		return response
 	}
+	response.NewPayloadUUID = newUUID
+	response.Success = true
+	return response
+
 }
 func processMythicRPCPayloadCreateFromScratch(msg amqp.Delivery) interface{} {
 	incomingMessage := MythicRPCPayloadCreateFromScratchMessage{}
