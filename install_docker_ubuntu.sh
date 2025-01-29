3c934d97580cfaa97c6f915228a263f45ffc57ed
Pidone <pd@secfault.org>
2025-01-29 15:56:55 +0100

    Fix Docker installation scripts
    apt-key is deprecated and already removed on kali.

--- install_docker_ubuntu.sh
+++ install_docker_ubuntu.sh
@@ -12,12 +12,15 @@ apt-get install -y \
     gnupg-agent \
     software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
 
-add-apt-repository -y \
-   "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu \
+echo \
+   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] \
+   https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
-   stable"
+   stable" > /etc/apt/sources.list.d/docker.list
 
 apt-get update
 
