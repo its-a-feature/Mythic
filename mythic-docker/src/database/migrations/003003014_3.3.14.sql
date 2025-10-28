c997ab3a8b354b9a742e3ca211b99d9e99fadc01
its-a-feature <codybthomas@gmail.com>
2025-10-28 09:56:59 -0500

    updating migrations

--- mythic-docker/src/database/migrations/003003014_3.3.14.sql
+++ mythic-docker/src/database/migrations/003003014_3.3.14.sql
@@ -4,7 +4,7 @@ alter table "public"."filemeta" add column IF NOT EXISTS "copy_of_file_id" integ
 alter table "public"."buildparameter" add column IF NOT EXISTS "ui_position" integer not null default 0;
 alter table "public"."buildparameter" add column IF NOT EXISTS "dynamic_query_function" text not null default ''::text;
 alter table "public"."c2profileparameters" add column IF NOT EXISTS "ui_position" integer not null default 0;
-alter table "public"."filemeta" drop constraint "filemeta_copy_of_file_id_fkey";
+alter table "public"."filemeta" drop constraint IF EXISTS "filemeta_copy_of_file_id_fkey";
 alter table "public"."filemeta" add constraint "filemeta_copy_of_file_id_fkey" FOREIGN KEY (copy_of_file_id) REFERENCES filemeta(id) ON UPDATE RESTRICT ON DELETE RESTRICT not valid;
 alter table "public"."filemeta" validate constraint "filemeta_copy_of_file_id_fkey";
 -- +migrate Down
