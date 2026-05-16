b5f982534d49cf181fd4909fedd7ac051b9615d6
its-a-feature <codybthomas@gmail.com>
2026-05-16 09:47:06 -0700

    better docs pages

--- documentation-docker/themes/hugo-theme-learn/static/js/hugo-learn.js
+++ documentation-docker/themes/hugo-theme-learn/static/js/hugo-learn.js
@@ -92,3 +92,72 @@ jQuery(document).ready(function() {
     $(element).parent().replaceWith('<div class="mermaid" align="center">' + content + '</div>');
   });
 });
+
+window.mythicDocsEnhanceHugoLearnContent = function() {
+  var images = $("div#body-inner img").not(".inline");
+
+  images.wrap(function(){
+    var image = $(this);
+    var o = getUrlParameter(image[0].src);
+    var f = o['featherlight'];
+    if (f != 'false') {
+      if (!image.parent("a").length) {
+        return "<a href='" + image[0].src + "' data-featherlight='image'></a>";
+      }
+    }
+  });
+
+  images.each(function(index){
+    var image = $(this)
+    var o = getUrlParameter(image[0].src);
+    if (typeof o !== "undefined") {
+      var h = o["height"];
+      var w = o["width"];
+      var c = o["classes"];
+      image.css("width", function() {
+        if (typeof w !== "undefined") {
+          return w;
+        } else {
+          return "auto";
+        }
+      });
+      image.css("height", function() {
+        if (typeof h !== "undefined") {
+          return h;
+        } else {
+          return "auto";
+        }
+      });
+      if (typeof c !== "undefined") {
+        var classes = c.split(',');
+        for (i = 0; i < classes.length; i++) {
+          image.addClass(classes[i]);
+        }
+      }
+    }
+  });
+
+  var text, clip = new ClipboardJS('.anchor');
+  $("h1~h2,h1~h3,h1~h4,h1~h5,h1~h6").append(function(index, html){
+    var element = $(this);
+    var url = encodeURI(document.location.origin + document.location.pathname);
+    var link = url + "#"+element[0].id;
+    return " <span class='anchor' data-clipboard-text='"+link+"'>" +
+      "<i class='fas fa-link fa-lg'></i>" +
+      "</span>"
+    ;
+  });
+
+  $(".anchor").on('mouseleave', function(e) {
+    $(this).attr('aria-label', null).removeClass('tooltipped tooltipped-s tooltipped-w');
+  });
+
+  clip.on('success', function(e) {
+      e.clearSelection();
+      $(e.trigger).attr('aria-label', 'Link copied to clipboard!').addClass('tooltipped tooltipped-s');
+  });
+  $('code.language-mermaid').each(function(index, element) {
+    var content = $(element).html().replace(/&amp;/g, '&');
+    $(element).parent().replaceWith('<div class="mermaid" align="center">' + content + '</div>');
+  });
+};
